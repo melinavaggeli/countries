19e46fddc1b9eaa9db1ab02c65b2b2bd561ae887
unknown <vaggelimelina@gmail.com>
2023-07-20 21:43:52 +0300

    added background img and styles

--- src/app/models/country.ts
+++ src/app/models/country.ts
@@ -1,6 +1,6 @@
-export type Country = {
+export interface Country {
   name: string;
   capital: string;
   flag: string;
   population: number;
-};
+}
--- src/app/pages/country-details/country-details.module.ts
+++ src/app/pages/country-details/country-details.module.ts
@@ -5,12 +5,14 @@ import { RouterModule } from '@angular/router';
 import { CountryDetailsComponent } from './country-details.component';
 import { CountryCardComponent } from 'src/app/components/country-card/country-card.component';
 
+import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 @NgModule({
   declarations: [CountryDetailsComponent, CountryCardComponent],
   imports: [
     CommonModule,
     MatSnackBarModule,
+    MatButtonModule,
     RouterModule.forChild([
       {
         path: '',
